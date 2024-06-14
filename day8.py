c7222c41306ed28594d5a71e3e7918febe34b763
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-14 09:08:45 +0000

    day8

---
+++ day8.py
@@ -0,0 +1,9 @@
+# Mengambil input dari pengguna
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+# Melakukan perkalian
+hasil = bilangan1 * bilangan2
+
+# Menampilkan hasil
+print("Hasil perkalian dari", bilangan1, "dan", bilangan2, "adalah", hasil)
