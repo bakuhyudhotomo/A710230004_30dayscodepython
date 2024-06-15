9b6628bbea2f71c9c435bb4cbe6a881988d76c94
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-15 13:16:30 +0000

    day9

---
+++ day9.py
@@ -0,0 +1,9 @@
+# Mendefinisikan dua bilangan
+bilangan1 = 10
+bilangan2 = 5
+
+# Melakukan pengurangan
+hasil_pengurangan = bilangan1 - bilangan2
+
+# Menampilkan hasil pengurangan
+print(f"Hasil pengurangan {bilangan1} - {bilangan2} adalah {hasil_pengurangan}")
