3aee273ff4f1f8919a7578d95d8205b4a4e8961a
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-16 11:25:50 +0000

    day10

---
+++ day10.py
@@ -0,0 +1,9 @@
+# Meminta input dari pengguna untuk dua bilangan
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+# Melakukan perkalian
+hasil = bilangan1 * bilangan2
+
+# Menampilkan hasil
+print(f"Hasil perkalian dari {bilangan1} dan {bilangan2} adalah {hasil}")
