f9e7759cd0003413c6d3df4e3fd16b81015bdd82
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-18 11:31:58 +0000

    day12

---
+++ day12.py
@@ -0,0 +1,9 @@
+# Meminta input dari pengguna
+numerator = float(input("Masukkan pembilang: "))
+denominator = float(input("Masukkan penyebut: "))
+
+# Melakukan pembagian
+result = numerator / denominator
+
+# Menampilkan hasil pembagian
+print("Hasil pembagian:", result)
