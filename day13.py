7e5406b8cae1334080ff7e6e36811cd6e8604dc0
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-19 01:32:37 +0000

    day13

---
+++ day13.py
@@ -0,0 +1,9 @@
+# Input dua bilangan
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+# Melakukan pembagian
+hasil = bilangan1 / bilangan2
+
+# Menampilkan hasil
+print("Hasil pembagian dari", bilangan1, "dan", bilangan2, "adalah:", hasil)
