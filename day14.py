c25e9e368446f89a228a19f75550551e0beb6f05
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-20 03:50:02 +0000

    day14

---
+++ day14.py
@@ -0,0 +1,6 @@
+def hitung_luas_lingkaran(jari_jari):
+    luas = 3.14 * jari_jari * jari_jari
+    return luas
+
+jari_jari = 5
+print("Luas lingkaran dengan jari-jari", jari_jari, "adalah", hitung_luas_lingkaran(jari_jari))
