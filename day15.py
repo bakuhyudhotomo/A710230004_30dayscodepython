fc5b4eed402570c07f408a297474f681100b2306
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-21 01:41:59 +0000

    day15

---
+++ day15.py
@@ -0,0 +1,10 @@
+# Variabel dan tipe data
+nama = "Bakuh"
+umur = 19
+tinggi = 1.65
+is_student = True
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Mahasiswa:", is_student)
