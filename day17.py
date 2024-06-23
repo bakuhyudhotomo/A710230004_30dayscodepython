b56aabd730b15ad48b9d6ebf3e35cbc02ad0eb5a
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-23 12:25:28 +0000

    day17

---
+++ day17.py
@@ -0,0 +1,11 @@
+# Loop menggunakan for
+print("Loop for:")
+for i in range(5):
+    print("Iterasi ke-", i)
+
+# Loop menggunakan while
+print("\nLoop while:")
+count = 0
+while count < 5:
+    print("Iterasi ke-", count)
+    count += 1
