f976e7094e8326e15d4d3e7812bd0acb87cf0f81
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-24 00:22:59 +0000

    day18

---
+++ day18.py
@@ -0,0 +1,13 @@
+# Fungsi untuk menambahkan dua angka
+def tambah(a, b):
+    return a + b
+
+# Fungsi untuk mengalikan dua angka
+def kali(a, b):
+    return a * b
+
+hasil_tambah = tambah(3, 4)
+hasil_kali = kali(3, 4)
+
+print("Hasil tambah:", hasil_tambah)
+print("Hasil kali:", hasil_kali)
