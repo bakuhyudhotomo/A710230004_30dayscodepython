69b7486bef608ab40bd48372c015415d9403066f
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-06-28 06:27:34 +0000

    day22

---
+++ day22.py
@@ -0,0 +1,9 @@
+# List
+buah = ["apel", "pisang", "jeruk"]
+for item in buah:
+    print("Buah:", item)
+
+# Dictionary
+nilai = {"Alice": 85, "Bob": 90, "Charlie": 78}
+for kunci, nilai in nilai.items():
+    print(kunci, "mendapat nilai", nilai)
