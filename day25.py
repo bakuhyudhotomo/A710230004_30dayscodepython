984d19e3b87730c04086bf80cf459d0fed01adb5
bakuhyudhotomo <mahesswara2010@gmail.com>
2024-07-01 12:52:05 +0000

    day25

---
+++ day25.py
@@ -0,0 +1,17 @@
+# Membuat list
+fruits = ["apple", "banana", "cherry"]
+
+# Mengakses elemen
+print(fruits[0])  # apple
+
+# Menambah elemen
+fruits.append("orange")
+print(fruits)
+
+# Menghapus elemen
+fruits.remove("banana")
+print(fruits)
+
+# Mengiterasi elemen
+for fruit in fruits:
+    print(fruit)
